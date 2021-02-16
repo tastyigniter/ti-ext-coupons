61f44baa0619a1819961dce0c14fac84870ab248
BreakSecurity <38836022+BreakSecurity@users.noreply.github.com>
2021-02-16 17:13:14 +0100

    Update CouponsTest.php

--- tests/Feature/Admin/Requests/CouponsTest.php
+++ tests/Feature/Admin/Requests/CouponsTest.php
@@ -165,7 +165,7 @@ class CouponTest extends TestCase
 						'fixed_date' => $faker->date(),
                     ],
                 ]),
-            ]
-        ]
+            ],
+        ];
     }
 }
