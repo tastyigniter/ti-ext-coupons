e4063cd8246608c213ccc790272e222bb0e1d62f
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2022-11-27 12:55:24 +0000

    Remove namespace on database migrations

--- database/migrations/2021_05_26_010000_alter_order_restriction_column.php
+++ database/migrations/2021_05_26_010000_alter_order_restriction_column.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace Igniter\Coupons\Database\Migrations;
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
