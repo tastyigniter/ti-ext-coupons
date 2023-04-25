71485d87b90802039fa80e23d59413df242a9df5
Sam <6567634+sampoyigi@users.noreply.github.com>
2023-04-25 14:57:04 +0100

    Fix class namespaces
    Signed-off-by: Sam <6567634+sampoyigi@users.noreply.github.com>

--- resources/models/coupon.php
+++ resources/models/coupon.php
@@ -314,7 +314,7 @@ $config['form']['tabs'] = [
             'type' => 'checkboxtoggle',
             'comment' => 'lang:igniter.coupons::default.help_order_restriction',
             'span' => 'right',
-            'options' => [\Admin\Models\Location::class, 'getOrderTypeOptions'],
+            'options' => [\Igniter\Admin\Models\Location::class, 'getOrderTypeOptions'],
         ],
         'redemptions' => [
             'tab' => 'lang:igniter.coupons::default.text_tab_restrictions',
