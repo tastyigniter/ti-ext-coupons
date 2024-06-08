7b10ca13a19bb4698874182ebabd0cedac33fc00
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2024-06-08 13:45:59 +0100

    Fix broken migration from laravel 11 upgrade
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- database/migrations/2021_09_06_010000_add_timestamps_to_coupons.php
+++ database/migrations/2021_09_06_010000_add_timestamps_to_coupons.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -13,6 +14,10 @@ return new class extends Migration
             $table->renameColumn('date_added', 'created_at');
             $table->timestamp('updated_at');
         });
+
+        DB::table('igniter_coupons')->update([
+            'updated_at' => DB::raw('created_at'),
+        ]);
 
         Schema::table('igniter_coupons_history', function(Blueprint $table) {
             $table->timestamp('date_used')->change();
