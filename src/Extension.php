tastyigniter/ti-ext-automation

Regroup permissions
Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

src/Extension.php
@@ -31,7 +31,7 @@ class Extension extends BaseExtension
         return [
             'Igniter.Automation.Manage' => [
                 'description' => 'Create, modify and delete automations',
-                'group' => 'module',
+                'group' => 'admin',
             ],
         ];
     }

tastyigniter/ti-ext-coupons

src/Extension.php
@@ -101,7 +101,7 @@ class Extension extends BaseExtension
         return [
             'Admin.Coupons' => [
                 'label' => 'igniter.coupons::default.permissions',
-                'group' => 'admin::lang.permissions.name',
+                'group' => 'order',
             ],
         ];
     }