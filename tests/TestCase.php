tastyigniter/ti-ext-automation

Fix tests, update phpstan-baseline.neon file with new error messages.
Signed-off-by: Sam <6567634+sampoyigi@users.noreply.github.com>

tests/TestCase.php
@@ -1,0 +1,14 @@
+<?php
+
+namespace Igniter\Automation\Tests;
+
+abstract class TestCase extends \Orchestra\Testbench\TestCase
+{
+    protected function getPackageProviders($app)
+    {
+        return [
+            \Igniter\Flame\ServiceProvider::class,
+            \Igniter\Automation\Extension::class,
+        ];
+    }
+}

tastyigniter/ti-ext-coupons

tests/TestCase.php
@@ -1,0 +1,14 @@
+<?php
+
+namespace Igniter\Coupons\Tests;
+
+abstract class TestCase extends \Orchestra\Testbench\TestCase
+{
+    protected function getPackageProviders($app)
+    {
+        return [
+            \Igniter\Flame\ServiceProvider::class,
+            \Igniter\Coupons\Extension::class,
+        ];
+    }
+}