2799fe4cdd4541189e7984c9e9177ba17b099be2
Obinna Elvis Okechukwu <dreamor47@gmail.com>
2025-07-18 23:40:51 +0100

    fix styling issue

--- resources/models/coupon_history.php
+++ resources/models/coupon_history.php
@@ -15,29 +15,29 @@ $config['list']['columns'] = [
         'searchable' => true,
     ],
     'code' => [
-        'label' => 'lang:igniter.coupons::default.column_code'
+        'label' => 'lang:igniter.coupons::default.column_code',
     ],
     'order_id' => [
-        'label' => 'lang:igniter.coupons::default.column_order_id'
+        'label' => 'lang:igniter.coupons::default.column_order_id',
     ],
     'customer_name' => [
-        'label' => 'lang:igniter.coupons::default.column_customer'
+        'label' => 'lang:igniter.coupons::default.column_customer',
     ],
     'min_total' => [
         'label' => 'lang:igniter.coupons::default.column_min_total',
-        'type' => 'currency'
+        'type' => 'currency',
     ],
     'amount' => [
         'label' => 'lang:igniter.coupons::default.column_amount',
-        'type' => 'currency'
+        'type' => 'currency',
     ],
     'created_at' => [
         'label' => 'lang:igniter.coupons::default.column_date_used',
-        'type' => 'datetime'
+        'type' => 'datetime',
     ],
     'coupon_history_id' => [
         'label' => 'lang:admin::lang.column_id',
-        'invisible' => true
+        'invisible' => true,
     ],
 ];
 
